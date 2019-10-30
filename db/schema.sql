ab5e754b7161224759487a355df886545f968172
Matt Michael <matthewemichael@gmail.com>
2019-10-30 14:36:55 -0500

    modified routing and UI

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NULL auto_increment,
-    burger_name VARCHAR(100) NOT NULL,
+    name VARCHAR(250) NOT NULL,
     devoured BOOL DEFAULT false,
     PRIMARY KEY (id)
 );
